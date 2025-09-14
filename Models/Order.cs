717620dc09d6e2485d64728e6718614e05e45d13
Justus78 <stars787878@aol.com>
2025-09-13 20:45:44 -0400

    added new model enum for order status, updated the order model to hold shipping info.

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using api.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 
 namespace CardShop.Models
@@ -7,31 +8,42 @@ namespace CardShop.Models
     {
         public int Id { get; set; }
 
+        // relationships
         [Required]
         public string? UserId { get; set; }
 
         [ValidateNever]
         public ApplicationUser User { get; set; }
 
-        [Required]
-        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        [ValidateNever]
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
+
+        // shipping info
+        public string RecipientName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+
+        // Payment / Metadata
+        public string PaymentIntentId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        // Status
         [Required]
         [StringLength(50)]
-        public string PaymentStatus { get; set; } = "Pending";
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
 
         [Required]
         [StringLength(50)]
         public string PaymentProvider { get; set; } = "Unspecified";
+        
 
+        // total amount of the order
         [Required]
-        [StringLength(100)]
-        public string? TransactionId { get; set; }
-
-        [ValidateNever]
-        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
-
-        [Required]
-        public decimal TotalAmount { get; set; }
+        public decimal? TotalAmount { get; set; }
     }
 }
---
+++ Models/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Models
+{
+    public enum OrderStatus
+    {
+        Pending,    // Created, awaiting payment
+        Paid,       // Payment succeeded
+        Shipped,    // Admin has shipped order
+        Completed,  // Delivered or confirmed
+        Cancelled   // Cancelled or refunded
+    } // end enum
+} // end model order status
