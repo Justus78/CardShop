81c5f46191e1997368418311feed51b31bedb279
Justus78 <stars787878@aol.com>
2025-10-10 20:25:56 -0400

    added email interface and service with one method to send mail

---
+++ Services/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using api.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using PostmarkDotNet;
+
+namespace api.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly string _apiKey;
+        private readonly string _fromEmail;
+
+        private EmailService(IConfiguration _config)
+        {
+            _apiKey = _config["Postmark:ApiKey"];
+            _fromEmail = _config["Postmark:FromEmail"];
+        }
+        public async Task SendEmailAsync(string to, string subject, string htmlBody)
+        {
+            var client = new PostmarkClient(_apiKey);
+
+            var message = new PostmarkMessage
+            {
+                From = _fromEmail,
+                To = to,
+                Subject = subject,
+                HtmlBody = htmlBody,
+                MessageStream = "outbound"
+            };
+
+            await client.SendMessageAsync(message);
+        }
+    }
+}
