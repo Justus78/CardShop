f902cc1d1b3add555ad04e93fb909cef22436aa0
Justus78 <stars787878@aol.com>
2025-11-02 20:55:18 -0500

    added User dto file and added user dto filr

---
+++ DTOs/User/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using api.DTOs.Order;
+
+namespace api.DTOs.User
+{
+    public class UserDto
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public List<OrderDto> Orders { get; set; } 
+
+    }
+}
